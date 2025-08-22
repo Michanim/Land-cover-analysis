1f8bc538d594207b6430527aea2fab4b64d94ea1
Michael Ofosu Anim <michanim0123@gmail.com>
2025-08-22 00:28:12 +0000

    Update Landcover.py

--- Landcover.py
+++ Landcover.py
@@ -150,7 +150,7 @@ st.markdown(
         border-left: 5px solid #0c5460;
     }
     .metric-card {
-        background: black;
+        background: red;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.1);
@@ -1473,4 +1473,5 @@ elif page == "⬇️ Downloads":
 
     except Exception as e:
         st.error(f"An error occurred while preparing downloads: {e}")
+
 
